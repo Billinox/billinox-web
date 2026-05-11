d88b34992f76e820c7f7854713361321d00fa1d4
Jude Iwuji <105007942+judeiwuji@users.noreply.github.com>
2026-05-11 11:01:17 +0100

    update: added netlify angular-runtime

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,8 @@ import {
 } from '@angular/ssr/node';
 import express from 'express';
 import { join } from 'node:path';
+import { getContext } from '@netlify/angular-runtime/context.mjs';
+import { createRequestHandler } from '@angular/ssr';
 
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
@@ -62,7 +64,24 @@ if (isMainModule(import.meta.url) || process.env['pm_id']) {
   });
 }
 
+export async function netlifyAppEngineHandler(
+  request: Request,
+): Promise<Response> {
+  const context = getContext();
+
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  const result = await angularApp.handle(request, context);
+  return result || new Response('Not found', { status: 404 });
+}
+
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
  */
-export const reqHandler = createNodeRequestHandler(app);
+// export const reqHandler = createNodeRequestHandler(app);
+export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
