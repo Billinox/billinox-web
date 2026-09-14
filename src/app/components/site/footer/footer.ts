a732c492207103b5e474af03ceaef5bbc9fefffd
Jude Iwuji <105007942+judeiwuji@users.noreply.github.com>
2026-09-14 18:06:03 +0100

    update: footer links to take sub links

--- src/app/components/site/footer/footer.ts
+++ src/app/components/site/footer/footer.ts
@@ -4,25 +4,38 @@ import { LucideMail } from '@lucide/angular';
 import { NgIcon } from '@ng-icons/core';
 import { socials } from '../../../constants/socials.constant';
 import { StoreBadge } from '../../shared/store-badge/store-badge';
+import { NgTemplateOutlet } from '@angular/common';
+
+interface FooterItem {
+  title: string;
+  links: { label: string; link: string; fragment?: string }[];
+}
 
 @Component({
   selector: 'app-footer',
-  imports: [LucideMail, NgIcon, RouterLink, StoreBadge],
+  imports: [LucideMail, NgIcon, RouterLink, StoreBadge, NgTemplateOutlet],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
-  public cols: {
-    title: string;
-    links: { label: string; link: string; fragment?: string }[];
-  }[] = [
-    {
-      title: 'Product',
-      links: [
-        { label: 'Features', link: '/', fragment: 'features' },
-        { label: 'Pricing', link: '/', fragment: 'pricing' },
-      ],
-    },
+  public cols: (FooterItem | FooterItem[])[] = [
+    [
+      {
+        title: 'Product',
+        links: [
+          { label: 'Features', link: '/', fragment: 'features' },
+          { label: 'Pricing', link: '/', fragment: 'pricing' },
+        ],
+      },
+      {
+        title: 'Free Tools',
+        links: [
+          { label: 'Invoice Generator', link: '/invoice-generator' },
+          { label: 'Receipt Generator', link: '/receipt-generator' },
+          { label: 'Quotation Generator', link: '/quotation-generator' },
+        ],
+      },
+    ],
     {
       title: 'Company',
       links: [
@@ -50,4 +63,6 @@ export class Footer {
 
   public socials = socials;
   public fullYear = new Date().getFullYear();
+
+  isArray = (value: any): boolean => Array.isArray(value);
 }
