4cb9f081db375a10e0d52d8e9e955c97c5ccb42a
Jude Iwuji <105007942+judeiwuji@users.noreply.github.com>
2026-09-16 08:10:43 +0100

    update: added page loading indicator

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
 
 import { HlmToasterImports } from '@spartan-ng/helm/sonner';
-import { LoadingBarModule } from '@ngx-loading-bar/core';
+import { LoadingBarModule, LoadingBarService } from '@ngx-loading-bar/core';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +18,31 @@ import { LoadingBarModule } from '@ngx-loading-bar/core';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected title = 'billinox';
+
+  private loadingBar = inject(LoadingBarService);
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter(
+          (e) =>
+            e instanceof NavigationEnd ||
+            e instanceof NavigationStart ||
+            e instanceof NavigationCancel ||
+            e instanceof NavigationError,
+        ),
+      )
+      .subscribe((e) => {
+        if (e instanceof NavigationStart) {
+          this.loadingBar.useRef().start();
+        }
+
+        if (e instanceof NavigationEnd) {
+          this.loadingBar.useRef().complete();
+        }
+      });
+  }
 }
